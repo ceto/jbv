744db15cd82a815af0882afdcbc84a0675355013
Gabor Szabo <szabogabi@gmail.com>
2014-10-06 18:44:15 +0200

    apartment single 24óra

--- lib/custom.php
+++ lib/custom.php
@@ -11,6 +11,15 @@ function jbv_slider_init() {
 		'singular_name'      => _x( 'Slide', 'roots' ),
 		'menu_name'          => _x( 'Slider', 'roots' ),
 		'name_admin_bar'     => _x( 'Slide', 'roots' ),
+    'add_new' => 'Add New Slide',
+    'add_new_item' => 'Add New Slide',
+    'edit_item' => 'Edit Slide',
+    'new_item' => 'New Slide',
+    'all_items' => 'All Slides',
+    'view_item' => 'View Slide',
+    'search_items' => 'Search Slides',
+    'not_found' =>  'No Slides found',
+    'not_found_in_trash' => 'No Slides found in Trash', 
 	);
 
 	$args = array(
@@ -44,7 +53,7 @@ function jbv_create_apartment() {
   $labels = array(
     'name' => 'Apartments',
     'singular_name' => 'Apartment',
-    'add_new' => 'Add New',
+    'add_new' => 'Add New Apartment',
     'add_new_item' => 'Add New Apartment',
     'edit_item' => 'Edit Apartment',
     'new_item' => 'New Apartment',
@@ -79,25 +88,129 @@ add_action( 'init', 'jbv_create_apartment' );
 /********* END OF Custom Post Types for Apartment Management ****************/
 
 
+/********* Custom MetaBoxes for Apartment Management ****************/
 
-/************* Custom Taxonomies for Apartment Management *********/
+/**
+ * Apartment Metaboxes
+*/
+add_filter( 'cmb_meta_boxes', 'jbv_apartment_meta' );
+function jbv_apartment_meta( array $meta_boxes ) {
+
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_meta_';
+
+  $meta_boxes[] = array(
+    'id'         => 'ameta',
+    'title'      => 'Apartment details',
+    'pages'      => array( 'apartment'), // Post type
+    'context'    => 'normal',
+    'priority'   => 'high',
+    'show_names' => true, // Show field names on the left
+    'fields'     => array(
+
+    array(
+      'name' => __('State'),
+      'id'   => $prefix . 'state',
+      'type' => 'radio_inline',
+      'options' => array(
+          array('name' => 'Fri', 'value' => 'fri',),
+          array('name' => 'Reserved', 'value' => 'reserved',),
+          array('name' => 'Utsolgt', 'value' => 'utsolgt',)
+      )
+    ),
+
+    array(
+      'name' => __('Floor'),
+      'id'   => $prefix . 'floor',
+      'type' => 'radio_inline',
+      'options' => array(
+          array('name' => 'EG', 'value' => 'EG',),
+          array('name' => '1. OG', 'value' => '1. OG',),
+          array('name' => '2. OG', 'value' => '2. OG',),
+          array('name' => '3. OG', 'value' => '3. OG',),
+          array('name' => '4. OG', 'value' => '4. OG',),
+          array('name' => '5. OG', 'value' => '5. OG',),
+          array('name' => '6. OG', 'value' => '6. OG',)
+      )
+    ),
+
+    array(
+        'name' => 'Område',
+        'id'   => $prefix . 'omr',
+        'type' => 'text_small',
+    ),
+    
+    array(
+        'name' => 'Pris',
+        'id'   => $prefix . 'pris',
+        'type' => 'text_small',
+    ),
+
+    array(
+      'name' => 'Floor map',
+      'desc' => 'Upload an image or enter an URL.',
+      'id' => $prefix . 'floormap',
+      'type' => 'file',
+      'save_id' => true, // save ID using true
+      'allow' => array( 'url', 'attachment' ) // limit to just attachments with array( 'attachment' )
+    ),
+
+    array(
+      'name' => 'Building schema',
+      'desc' => 'Upload an image or enter an URL.',
+      'id' => $prefix . 'schema',
+      'type' => 'file',
+      'save_id' => true, // save ID using true
+      'allow' => array( 'url', 'attachment' ) // limit to just attachments with array( 'attachment' )
+    ),
+
+    array(
+      'name' => 'PDF to download',
+      'desc' => 'Upload a PDF Document.',
+      'id' => $prefix . 'pdf',
+      'type' => 'file',
+      'save_id' => true, 
+      'allow' => array( 'url', 'attachment' ) // limit to just attachments with array( 'attachment' )
+    ),
+
+    array(
+        'name' => 'SVG Definition',
+        'desc' => 'Experimental! Do not change it!',
+        'id'   => $prefix . 'svgdata',
+        'type' => 'textarea_small',
+    ),
+   
+    ),
+  );
+
+
+  return $meta_boxes;
+}
+
+/********* End of Custom MetaBoxes for Apartment Management ****************/
+
+
+
+
+
+/************* Custom Object Taxonomies for Apartment Management *********/
 
 add_action( 'init', 'jbv_create_object_taxonomies', 0 );
 
 function jbv_create_object_taxonomies() {
   // Add new taxonomy, make it hierarchical (like categories)
   $labels = array(
-    'name'              => _x( 'Objects', 'taxonomy general name' ),
-    'singular_name'     => _x( 'Object', 'taxonomy singular name' ),
-    'search_items'      => __( 'Search Objects' ),
-    'all_items'         => __( 'All Objects' ),
-    'parent_item'       => __( 'Parent Object' ),
-    'parent_item_colon' => __( 'Parent Object:' ),
-    'edit_item'         => __( 'Edit Object' ),
-    'update_item'       => __( 'Update Object' ),
-    'add_new_item'      => __( 'Add New Object' ),
-    'new_item_name'     => __( 'New Object Name' ),
-    'menu_name'         => __( 'Object' ),
+    'name'              => _x( 'Buildings', 'taxonomy general name' ),
+    'singular_name'     => _x( 'Building', 'taxonomy singular name' ),
+    'search_items'      => __( 'Search Buildings' ),
+    'all_items'         => __( 'All Buildings' ),
+    'parent_item'       => __( 'Parent Building' ),
+    'parent_item_colon' => __( 'Parent Building:' ),
+    'edit_item'         => __( 'Edit Building' ),
+    'update_item'       => __( 'Update Building' ),
+    'add_new_item'      => __( 'Add New Building' ),
+    'new_item_name'     => __( 'New Building Name' ),
+    'menu_name'         => __( 'Buildings' ),
   );
 
   $args = array(
@@ -106,15 +219,45 @@ function jbv_create_object_taxonomies() {
     'show_ui'           => true,
     'show_admin_column' => true,
     'query_var'         => true,
-    'rewrite'           => array( 'slug' => 'object' ),
+    'rewrite'           => array( 'slug' => 'building' ),
   );
 
   register_taxonomy( 'object', array( 'apartment' ), $args );
 }
 
-/********* END OF Custom Taxonomies for Apartment Management ****************/
+/********* END OF Custom Object Taxonomies for Apartment Management ****************/
 
 
+
+/********* Custom Tax Meta for Object Taxonomy ****************/
+require_once("TMC/Tax-meta-class.php");
+if (is_admin()){
+  $prefix = '_meta_';
+  $config = array(
+    'id' => 'ometa',          // meta box id, unique per meta box
+    'title' => 'Additional details for building and floor',          // meta box title
+    'pages' => array('object'),        // taxonomy name, accept categories, post_tag and custom taxonomies
+    'context' => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+    'fields' => array(),            // list of meta fields (can be added by field arrays)
+    'local_images' => false,          // Use local or hosted images (meta box images for add/remove)
+    'use_with_theme' => '/wp-content/themes/jbv/lib/TMC'          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+  );
+  $my_meta =  new Tax_Meta_Class($config);
+  $my_meta->addTextarea($prefix.'svgdata',array('name'=> __('SVG Data','roots')));
+  $my_meta->addImage($prefix.'image',array('name'=> __('Image of this item ','roots')));
+  //$my_meta->addWysiwyg($prefix.'content',array('name'=> __('Content editor ','tax-meta')));
+  $my_meta->Finish();
+}
+
+/********* END OF Custom Tax Meta for Object Taxonomy ****************/
+
+
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+function cmb_initialize_cmb_meta_boxes() {
+  if ( ! class_exists( 'cmb_Meta_Box' ) )
+    require_once 'CMB/init.php';
+
+}
 
 
 
---
+++ templates/apartment-single.php
@@ -0,0 +1,61 @@
+<header class="page-hero apartment-hero">
+  <div class="wrapper wrapper-fullwidth">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo_kicsike.png" alt="JORDBÆRVEGEN" class="watermark">
+  </div>
+</header>
+<div class="wrapper wrapper-fullwidth wrapper-apartment">
+  <div class="page-header">
+    <h2>Finn Din Bolig</h2>
+  </div>
+  <div class="apartment-details">
+
+
+    <div class="apartment-details--left">
+      <div class="apartment-details--left--inner">
+        <figure class="apartment-3dfloormap">
+          <?php $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large');  ?>
+          <a class="popup-zoom" href="<?php echo $imgsrc[0]; ?>">
+            <?php the_post_thumbnail('large'); ?>
+          </a>
+        </figure>
+      </div>
+    </div>
+
+
+
+    <div class="apartment-details--right">
+      
+      <div class="apartment-details--right--inner-white">
+        <div class="holdit">
+          <figure class="apartment-2dfloormap">
+            <a class="popup-zoom" href="<?php echo get_post_meta($post->ID, '_meta_floormap', true); ?>">
+              <img src="<?php echo get_post_meta($post->ID, '_meta_floormap', true); ?>" />
+            </a>
+          </figure>
+          <figure class="apartment-schema">
+            <a href="#">
+              <img src="<?php echo get_post_meta($post->ID, '_meta_schema', true); ?>" />
+            </a>
+          </figure>
+        </div>
+      </div>
+
+      <div class="apartment-details--right--inner-red">
+        <div class="holdit">
+          <div class="apartment-facts">
+            <p class="data-item"><span>Navn</span> <?php the_title(); ?></p>
+            <p class="data-item"><span>Floor</span> <?php echo get_post_meta( $post->ID, '_meta_floor', true ); ?></p>
+            <p class="data-item"><span>Område</span> <?php echo get_post_meta( $post->ID, '_meta_omr', true ); ?> m<sup>2</sup></p>
+            <p class="price data-item"><span>Pris</span> <?php echo number_format(get_post_meta( $post->ID, '_meta_pris', true ), 0, ',', ' '); ?> NOK</p>
+            <p class="data-item"><span>State</span> <?php echo get_post_meta( $post->ID, '_meta_state', true ); ?></p>
+            
+          </div>
+          <div class="apartment-discl">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
